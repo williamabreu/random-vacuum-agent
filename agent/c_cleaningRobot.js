6b6c4eb52350ee77c9de58a1af8eb5f45b422243
williamabreu <williamsantos_00@hotmail.com>
2018-09-08 23:59:16 -0300

    Iniciado o projeto

---
+++ agent/c_cleaningRobot.js
@@ -0,0 +1,271 @@
+/* The general structure is to put the AI code in xyz.js and the visualization
+   code in c_xyz.js. Create a diagram object that contains all the information
+   needed to draw the diagram, including references to the environment&agents.
+   Then use a draw function to update the visualization to match the data in
+   the environment & agent objects. Use a separate function if possible for 
+   controlling the visualization (whether through interaction or animation). 
+   Chapter 2 has minimal AI and is mostly animations. */
+
+const SIZE = 100;
+const colors = {
+    perceptBackground: 'hsl(240,10%,85%)',
+    perceptHighlight: 'hsl(60,100%,90%)',
+    actionBackground: 'hsl(0,0%,100%)',
+    actionHighlight: 'hsl(150,50%,80%)'
+};
+
+
+/* Position Mapping */
+function xPosition(floorNumber) {
+    return {0: 20, 1: 170, 2: 170, 3: 20}[floorNumber];
+}
+
+function yPosition(floorNumber) {
+    return {0: 450, 1: 450, 2: 225, 3: 225}[floorNumber] - 120;
+}
+
+
+/* Create a diagram object that includes the world (model) and the svg
+   elements (view) */
+function makeDiagram(selector) {
+    let diagram = {}, world = new World(4);
+    diagram.world = world;
+
+    diagram.root = d3.select(selector);
+
+    diagram.robot = diagram.root.append('g')
+        .attr('class', 'robot')
+        .style('transform', `translate(${xPosition(world.location)}px,${yPosition(world.location)}px)`);
+    diagram.robot.append('rect')
+        .attr('width', SIZE)
+        .attr('height', SIZE)
+        .attr('fill', 'hsl(120,25%,50%)');
+    diagram.perceptText = diagram.robot.append('text')
+        .attr('x', SIZE/2)
+        .attr('y', -25)
+        .attr('text-anchor', 'middle');
+    diagram.actionText = diagram.robot.append('text')
+        .attr('x', SIZE/2)
+        .attr('y', -10)
+        .attr('text-anchor', 'middle');
+
+    diagram.floors = [];
+    for (let floorNumber = 0; floorNumber < world.floors.length; floorNumber++) {
+        diagram.floors[floorNumber] =
+            diagram.root.append('rect')
+            .attr('class', 'clean floor') // for css
+            .attr('width', SIZE)
+            .attr('height', SIZE/4)
+            .attr('stroke', 'black');
+            // .on('click', function() {
+            //     world.markFloorDirty(floorNumber);
+            //     diagram.floors[floorNumber].attr('class', 'dirty floor');
+            // });
+    }
+
+    diagram.floors[0]
+        .attr('x', 20)
+        .attr('y', 450);
+    diagram.floors[1]
+        .attr('x', 170)
+        .attr('y', 450);
+    diagram.floors[2]
+        .attr('x', 170)
+        .attr('y', 225);
+    diagram.floors[3]
+        .attr('x', 20)
+        .attr('y', 225);
+
+    return diagram;
+}
+
+
+/* Rendering functions read from the state of the world (diagram.world) 
+   and write to the state of the diagram (diagram.*). For most diagrams
+   we only need one render function. For the vacuum cleaner example, to
+   support the different styles (reader driven, agent driven) and the
+   animation (agent perceives world, then pauses, then agent acts) I've
+   broken up the render function into several. */
+
+function renderWorld(diagram) {
+    for (let floorNumber = 0; floorNumber < diagram.world.floors.length; floorNumber++) {
+        diagram.floors[floorNumber].attr('class', diagram.world.floors[floorNumber].dirty? 'dirty floor' : 'clean floor');
+    }
+    diagram.robot.style('transform', `translate(${xPosition(diagram.world.location)}px,${yPosition(diagram.world.location)}px)`);
+}
+
+function renderAgentPercept(diagram, dirty) {
+    let perceptLabel = {false: "It's clean", true: "It's dirty"}[dirty];
+    diagram.perceptText.text(perceptLabel);
+}
+
+function renderAgentAction(diagram, action) {
+    let actionLabel = {null: 'Waiting', 'SUCK': 'Vacuuming', 'LEFT': 'Going left', 'RIGHT': 'Going right', 'UP': 'Going up', 'DOWN': 'Going down'}[action];
+    diagram.actionText.text(actionLabel);
+}
+
+
+/* Control the diagram by letting the AI agent choose the action. This
+   controller is simple. Every STEP_TIME_MS milliseconds choose an
+   action, simulate the action in the world, and draw the action on
+   the page. */
+
+const STEP_TIME_MS = 2500;
+function makeAgentControlledDiagram() {
+    let diagram = makeDiagram('#agent-controlled-diagram svg');
+
+    function update() {
+        let location = diagram.world.location;
+        let percept = diagram.world.floors[location].dirty;
+        let action = reflexVacuumAgent(diagram.world);
+        diagram.world.simulate(action);
+        renderWorld(diagram);
+        renderAgentPercept(diagram, percept);
+        renderAgentAction(diagram, action);
+    }
+
+    function randomDirty() {
+        let max = 3, min = 0;
+        let floorNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        diagram.world.markFloorDirty(floorNumber);
+        diagram.floors[floorNumber].attr('class', 'dirty floor');
+    }
+
+    update();
+    setInterval(update, STEP_TIME_MS);
+    randomDirty();
+    setInterval(randomDirty, STEP_TIME_MS * 1.5);
+}
+
+
+/* Control the diagram by letting the reader choose the action. This
+   diagram is tricky.
+ 
+   1. If there's an animation already playing and the reader chooses
+      an action then *wait* for the animation to finish playing. While
+      waiting the reader may choose a different action. Replace the
+      previously chosen action with the new one. (An alternative
+      design would be to queue up all the actions.)
+   2. If there's not an animation already playing then when the reader
+      chooses an action then run it right away, without waiting.
+   3. Show the connection between the percept and the resulting action
+      by highlighting the percepts in the accompanying table, pausing,
+      and then highlighting the action.
+*/
+// function makeReaderControlledDiagram() {
+//     let diagram = makeDiagram('#reader-controlled-diagram svg');
+//     let nextAction = null;
+//     let animating = false; // either false or a setTimeout intervalID
+
+//     function makeButton(action, label, x) {
+//         let button = d3.select('#reader-controlled-diagram .buttons')
+//             .append('button')
+//             .attr('class', 'btn btn-default')
+//             .style('position', 'absolute')
+//             .style('left', x + 'px')
+//             .style('width', '100px')
+//             .text(label)
+//             .on('click', () => {
+//                 setAction(action);
+//                 updateButtons();
+//             });
+//         button.action = action;
+//         return button;
+//     }
+
+//     let buttons = [
+//         makeButton('LEFT', 'Move left', 150),
+//         makeButton('SUCK', 'Vacuum', 300),
+//         makeButton('RIGHT', 'Move right', 450),
+//     ];
+
+//     function updateButtons() {
+//         for (let button of buttons) {
+//             button.classed('btn-warning', button.action == nextAction);
+//         }
+//     }
+
+//     function setAction(action) {
+//         nextAction = action;
+//         if (!animating) { update(); }
+//     }
+    
+//     function update() {
+//         let percept = diagram.world.floors[diagram.world.location].dirty;
+//         if (nextAction !== null) {
+//             diagram.world.simulate(nextAction);
+//             renderWorld(diagram);
+//             renderAgentPercept(diagram, percept);
+//             renderAgentAction(diagram, nextAction);
+//             nextAction = null;
+//             updateButtons();
+//             animating = setTimeout(update, STEP_TIME_MS);
+//         } else {
+//             animating = false;
+//             renderWorld(diagram);
+//             renderAgentPercept(diagram, percept);
+//             renderAgentAction(diagram, null);
+//         }
+//     }
+// }
+
+
+/* Control the diagram by letting the reader choose the rules that
+   the AI agent should follow. The animation flow is similar to the
+   first agent controlled diagram but there is an additional table
+   UI that lets the reader view the percepts and actions being followed
+   as well as change the rules followed by the agent. */
+// function makeTableControlledDiagram() {
+//     let diagram = makeDiagram('#table-controlled-diagram svg');
+
+//     function update() {
+//         let table = getRulesFromPage();
+//         let location = diagram.world.location;
+//         let percept = diagram.world.floors[location].dirty;
+//         let action = tableVacuumAgent(diagram.world, table);
+//         diagram.world.simulate(action);
+//         renderWorld(diagram);
+//         renderAgentPercept(diagram, percept);
+//         renderAgentAction(diagram, action);
+//         showPerceptAndAction(location, percept, action);
+//     }
+//     update();
+//     setInterval(update, STEP_TIME_MS);
+    
+//     function getRulesFromPage() {
+//         let table = d3.select("#table-controlled-diagram table");
+//         let left_clean = table.select("[data-action=left-clean] select").node().value;
+//         let left_dirty = table.select("[data-action=left-dirty] select").node().value;
+//         let right_clean = table.select("[data-action=right-clean] select").node().value;
+//         let right_dirty = table.select("[data-action=right-dirty] select").node().value;
+//         return [[left_clean, left_dirty], [right_clean, right_dirty]];
+//     }
+
+//     function showPerceptAndAction(location, percept, action) {
+//         let locationMarker = location? 'right' : 'left';
+//         let perceptMarker = percept? 'dirty' : 'clean';
+        
+//         d3.selectAll('#table-controlled-diagram th')
+//             .filter(function() {
+//                 let marker = d3.select(this).attr('data-input');
+//                 return marker == perceptMarker || marker == locationMarker;
+//             })
+//             .style('background-color', (d) => colors.perceptHighlight);
+        
+//         d3.selectAll('#table-controlled-diagram td')
+//             .style('padding', '5px')
+//             .filter(function() {
+//                 let marker = d3.select(this).attr('data-action');
+//                 return marker == locationMarker + '-' + perceptMarker;
+//             })
+//             .transition().duration(0.05 * STEP_TIME_MS)
+//             .style('background-color', colors.actionHighlight)
+//             .transition().duration(0.9 * STEP_TIME_MS)
+//             .style('background-color', colors.actionBackground);
+//     }
+// }
+
+
+makeAgentControlledDiagram();
+// makeReaderControlledDiagram();
+// makeTableControlledDiagram();
---
+++ agent/cleaningRobot.js
@@ -0,0 +1,63 @@
+// In this simple problem the world includes both the environment and the robot
+// but in most problems the environment and world would be separate
+class World {
+    constructor(numFloors) {
+        this.location = 0;
+        this.floors = [];
+        for (let i = 0; i < numFloors; i++) {
+            this.floors.push({dirty: false});
+        }
+    }
+
+    markFloorDirty(floorNumber) {
+        this.floors[floorNumber].dirty = true;
+    }
+
+    simulate(action) {
+        switch(action) {
+            case 'SUCK':
+                this.floors[this.location].dirty = false;
+                break;
+            case 'LEFT':
+                this.location = 3;
+                break;
+            case 'RIGHT':
+                this.location = 1;
+                break;
+            case 'UP':
+                this.location = 2;
+                break;
+            case 'DOWN':
+                this.location = 0;
+                break;
+        }
+        return action;
+    }
+}
+
+
+// Rules are defined in code
+function reflexVacuumAgent(world) {
+    if (world.floors[world.location].dirty) { 
+        return 'SUCK'; 
+    }
+    else {
+        switch (world.location) {
+            case 0:
+                return 'RIGHT';
+            case 1:
+                return 'UP';
+            case 2:
+                return 'LEFT';
+            case 3:
+                return 'DOWN';
+        }         
+    }
+}
+
+// Rules are defined in data, in a table indexed by [location][dirty]
+// function tableVacuumAgent(world, table) {
+//     let location = world.location;
+//     let dirty = world.floors[location].dirty ? 1 : 0;
+//     return table[location][dirty];
+// }
